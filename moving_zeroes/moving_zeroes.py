f52ea27e84924f5a17a9ffcad6a70205ebaf80e4
kly001 <karenly1961@gmail.com>
2020-06-10 13:00:32 -0400

    moving_zeroes.py: completed

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -7,14 +7,10 @@ def moving_zeroes(arr):
     n = len(arr)
     count = 0
 
-    for i in range(n):
-        if arr[i] != 0:
-            arr[count] = arr[i]
-            count =+ 1
-    while count < n:
-        arr[count] = 0
-        count += 1
-
+    for i in range(0, n):
+        if (arr[i] != 0):
+            arr[count], arr[i] = arr[i], arr[count]
+            count += 1
     return arr
 
 if __name__ == '__main__':
