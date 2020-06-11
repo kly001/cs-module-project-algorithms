f69348c8722ae6b3b1d8b0c40141b022937554a8
kly001 <karenly1961@gmail.com>
2020-06-11 18:17:36 -0400

    eating_cookies.py: solution from lecture w/Sean C.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,18 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+# base case: when there are no more cookies
+    if n == 0:
+        return 1
 
-    pass
+# check for negative n values
+    elif n <= 0:
+        return 0
+
+# this represents our recursive case where there are still cookies to be eaten
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+   
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
